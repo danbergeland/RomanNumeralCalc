3d4e36a319734f7f552770462fffc122b7b9c734
Dan <danbergeland@gmail.com>
2017-06-10 12:52:17 -0500

    added Numeral class

--- NumeralTest.py
+++ NumeralTest.py
@@ -1,6 +1,14 @@
 import unittest
-
+import numeral
     
 class UnittestTestCase(unittest.TestCase):
     def runTest(self):
         assert True
+    
+class NumeralTestCaseSetup(unittest.TestCase):
+    def setUp(self):
+        self.numeral_ = numeral.Numeral('I')
+        
+class NumeralValueIis1(NumeralTestCaseSetup):
+    def runTest(self):
+        assert self.numeral_.value == 1
---
+++ numeral.py
@@ -0,0 +1,10 @@
+
+
+class Numeral:
+    def __init__(self,numeralLetter):
+        self.letter = numeralLetter
+        self.value = 1
+        
+    
+
+    
