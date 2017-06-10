4b2a5f4a179c46bb2fb24ff26d7a6b7529142ec1
Dan <danbergeland@gmail.com>
2017-06-10 12:55:43 -0500

    added test for letter/number

--- NumeralTest.py
+++ NumeralTest.py
@@ -9,6 +9,10 @@ class NumeralTestCaseSetup(unittest.TestCase):
     def setUp(self):
         self.numeral_ = numeral.Numeral('I')
         
+class NumeralLetterIisI(NumeralTestCaseSetup):
+    def runTest(self):
+        assert self.numeral_.letter == 'I'
+        
 class NumeralValueIis1(NumeralTestCaseSetup):
     def runTest(self):
         assert self.numeral_.value == 1
